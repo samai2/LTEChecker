99ae2ff054898d0181a5dde3b32ba25cf7d43f2e
Maxim Shadrin <maxim.shadrin@al-enterprise.com>
2023-10-13 17:13:07 +0200

    Init commit

---
+++ app/src/main/java/com/ale/ltechecker/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.ale.ltechecker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ale.ltechecker.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+    }
+
+
+}
+
