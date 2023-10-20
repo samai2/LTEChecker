90c0c46f757797fd1b860cfd30070c6e23911fb3
Maxim Shadrin <maxim.shadrin@al-enterprise.com>
2023-10-20 11:13:45 +0200

    works

--- app/src/main/java/com/ale/ltechecker/ui/DisplayCollectedInfoActivity.kt
+++ app/src/main/java/com/ale/ltechecker/ui/DisplayCollectedInfoActivity.kt
@@ -2,6 +2,7 @@ package com.ale.ltechecker.ui
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
@@ -27,20 +28,25 @@ class DisplayCollectedInfoActivity : AppCompatActivity() {
     }
 
     private var binding: ActivityDisplayCollectedInfoBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val infoBinding = ActivityDisplayCollectedInfoBinding.inflate(layoutInflater)
         binding = infoBinding
-        val isTestPassed = intent.getBooleanExtra(TEXT_RESULT_EXTRA, false)
-        displayInfoToSend(isTestPassed)
+
+
         setContentView(infoBinding.root)
+
+        displayInfoToSend()
+
+        setupSendInfoButton()
+    }
+
+    private fun setupSendInfoButton() {
         binding?.sendInfo?.setOnClickListener {
-            val uriToImage1 = sentInfo(binding?.displayInfo?.text.toString()) ?: return@setOnClickListener
-            val uriToImage = FileProvider.getUriForFile(
-                this, "${BuildConfig.APPLICATION_ID}.provider", File(uriToImage1)
-            )
+            val infoFileUri = getUriToSend(writeTextToFile(binding?.displayInfo?.text.toString())) ?: return@setOnClickListener
             with(Intent(Intent.ACTION_SEND_MULTIPLE)) {
-                setDataAndType(uriToImage, "*/*")
+                setDataAndType(infoFileUri, "*/*")
                 putExtra(Intent.EXTRA_SUBJECT, "Phone info")
                 addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
                 addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
@@ -55,7 +61,9 @@ class DisplayCollectedInfoActivity : AppCompatActivity() {
     }
 
 
-    private fun displayInfoToSend(isTestPassed: Boolean) {
+    private fun displayInfoToSend() {
+        val isTestPassed = intent.getBooleanExtra(TEXT_RESULT_EXTRA, false)
+
         val phoneInfo = PhoneInfo()
         val infoToSend = """
             ${phoneInfo.collectPhoneInfo(isTestPassed)}
@@ -63,11 +71,18 @@ class DisplayCollectedInfoActivity : AppCompatActivity() {
         binding?.displayInfo?.text = infoToSend
     }
 
-    fun sentInfo(jsonText: String): URI? {
+    private fun writeTextToFile(jsonText: String): URI? {
         val pathToCacheFile = externalCacheDir?.absolutePath ?: return null
         val infoFile = Path(pathToCacheFile, "report.json")
         infoFile.writeText(jsonText)
         return infoFile.toUri()
+    }
+
+    private fun getUriToSend(uri: URI?): Uri? {
+        uri ?: return null
+        return FileProvider.getUriForFile(
+            this, "${BuildConfig.APPLICATION_ID}.provider", File(uri)
+        )
     }
 }
 
