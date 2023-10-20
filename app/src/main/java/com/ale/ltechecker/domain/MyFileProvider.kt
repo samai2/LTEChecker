703b8d37bd066e9980f076a180b2597990d2d1b9
Maxim Shadrin <maxim.shadrin@al-enterprise.com>
2023-10-20 14:05:18 +0200

    works

--- app/src/main/java/com/ale/ltechecker/domain/MyFileProvider.kt
+++
@@ -1,6 +0,0 @@
-package com.ale.ltechecker.domain
-
-import androidx.core.content.FileProvider
-import com.ale.ltechecker.R
-
-class MyFileProvider : FileProvider(R.xml.file_paths)
--- app/src/main/java/com/ale/ltechecker/domain/phonestate/PhoneStateInfo.kt
+++ app/src/main/java/com/ale/ltechecker/domain/phonestate/PhoneStateInfo.kt
@@ -4,13 +4,15 @@ import android.content.Context
 import android.net.ConnectivityManager
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class PhoneStateInfo(context: Context) {
 
-    private val observeNetworkChanged = CoroutineScope(Dispatchers.IO)
+    private val observeNetworkChanged = CoroutineScope(Dispatchers.IO + SupervisorJob())
     private val connectivityManager = context.getSystemService(ConnectivityManager::class.java)
     private val networkChangedCallback = NetworkChangedCallback()
 
@@ -20,13 +22,20 @@ class PhoneStateInfo(context: Context) {
     val wifiStatus = MutableStateFlow(wifiChecker.getWiFiStatus())
     val lteTestStatus = MutableStateFlow(LTEState.UNKNOWN)
 
-
     init {
 
         connectivityManager.registerDefaultNetworkCallback(networkChangedCallback)
         observeNetworkChanged.launch {
             networkChangedCallback.networkChangeEvents.collect {
                 refreshPhoneStateInfo()
+            }
+        }
+        //Durty work around in case when we deal with bug,
+        // what we cannot get LTE connection via Connectivity Manager
+        observeNetworkChanged.launch {
+            while (true) {
+                launch { lteTestStatus.emit(lteChecker.checkLTEState()) }
+                delay(2000)
             }
         }
     }
