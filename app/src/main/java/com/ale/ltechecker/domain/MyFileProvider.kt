ae9d44b3d775dbb466ca53dd79ce8339426ead08
Maxim Shadrin <maxim.shadrin@al-enterprise.com>
2023-10-20 09:59:03 +0200

    works

---
+++ app/src/main/java/com/ale/ltechecker/domain/MyFileProvider.kt
@@ -0,0 +1,6 @@
+package com.ale.ltechecker.domain
+
+import androidx.core.content.FileProvider
+import com.ale.ltechecker.R
+
+class MyFileProvider : FileProvider(R.xml.file_paths)
--- app/src/main/java/com/ale/ltechecker/ui/DisplayCollectedInfoActivity.kt
+++ app/src/main/java/com/ale/ltechecker/ui/DisplayCollectedInfoActivity.kt
@@ -4,12 +4,16 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import com.ale.ltechecker.BuildConfig
+import com.ale.ltechecker.R
 import com.ale.ltechecker.databinding.ActivityDisplayCollectedInfoBinding
 import com.ale.ltechecker.domain.phoneInfo.PhoneInfo
+import java.io.File
 import java.net.URI
 import kotlin.io.path.Path
 import kotlin.io.path.writeText
+
 
 class DisplayCollectedInfoActivity : AppCompatActivity() {
     companion object {
@@ -30,8 +34,25 @@ class DisplayCollectedInfoActivity : AppCompatActivity() {
         val isTestPassed = intent.getBooleanExtra(TEXT_RESULT_EXTRA, false)
         displayInfoToSend(isTestPassed)
         setContentView(infoBinding.root)
+        binding?.sendInfo?.setOnClickListener {
+            val uriToImage1 = sentInfo(binding?.displayInfo?.text.toString()) ?: return@setOnClickListener
+            val uriToImage = FileProvider.getUriForFile(
+                this, "${BuildConfig.APPLICATION_ID}.provider", File(uriToImage1)
+            )
+            with(Intent(Intent.ACTION_SEND_MULTIPLE)) {
+                setDataAndType(uriToImage, "*/*")
+                putExtra(Intent.EXTRA_SUBJECT, "Phone info")
+                addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+                putParcelableArrayListExtra(Intent.EXTRA_STREAM, arrayListOf(data))
+                this@DisplayCollectedInfoActivity.startActivity(
+                    Intent.createChooser(
+                        this, this@DisplayCollectedInfoActivity.getString(R.string.choose_email_client)
+                    )
+                )
+            }
+        }
     }
-
 
 
     private fun displayInfoToSend(isTestPassed: Boolean) {
@@ -42,11 +63,11 @@ class DisplayCollectedInfoActivity : AppCompatActivity() {
         binding?.displayInfo?.text = infoToSend
     }
 
-    fun sentInfo(jsonText: String) : URI? {
+    fun sentInfo(jsonText: String): URI? {
         val pathToCacheFile = externalCacheDir?.absolutePath ?: return null
         val infoFile = Path(pathToCacheFile, "report.json")
         infoFile.writeText(jsonText)
         return infoFile.toUri()
-
     }
 }
+
